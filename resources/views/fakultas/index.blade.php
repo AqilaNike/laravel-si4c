5c3773b4c8f95c75e084b4737fea64b8c32fa858
AqilaNike <aqilanikeindriani_2428240130@mhs.mdp.ac.id>
2026-05-11 13:11:55 +0700

    tambahin adminlte

--- resources/views/fakultas/index.blade.php
+++ resources/views/fakultas/index.blade.php
@@ -1,4 +1,24 @@
-<h2>Data Fakultas</h2>
-@foreach ($result as $item)
-    {{ $item->nama }} - {{ $item->singkatan }} - {{ $item->dekan }} <br>
-@endforeach
+@extends('main')
+
+@section('title', 'Fakultas')
+
+@section('content')
+<table class="table table-bordered table-hover">
+    <thead>
+        <tr>
+            <th>Nama</th>
+            <th>Singkatan</th>
+            <th>Dekan</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($result as $item)
+            <tr>
+                <td>{{ $item->nama }}</td>
+                <td>{{ $item->singkatan }}</td>
+                <td>{{ $item->dekan }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+@endsection
