19622ebfcf7cb12d1d9d553fdb14106afccc1653
AqilaNike <aqilanikeindriani_2428240130@mhs.mdp.ac.id>
2026-06-09 12:25:57 +0700

    baru lagi

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,28 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\FakultasController;
 use App\Http\Controllers\PeriodeController;
 use App\Http\Controllers\BeritaController;
 use App\Http\Controllers\ProdiController;
 use App\Http\Controllers\MahasiswaController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 
-Route::get('/', function () {
-    return view('fakultas.create');
+Route::get('/', fn() => redirect()->route('dashboard'));
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('fakultas', FakultasController::class);
+    Route::resource('periode', PeriodeController::class);
+    Route::resource('berita', BeritaController::class);
+    Route::resource('prodi', ProdiController::class);
+    Route::resource('mahasiswa', MahasiswaController::class);
 });
 
-Route::get('/', fn() => redirect()->route('dashboard'));
-Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
-
-Route::resource('/fakultas', FakultasController::class)
-    ->parameters(['fakultas' => 'fakultas']);
-Route::resource('/periode', PeriodeController::class);
-Route::resource('/berita', BeritaController::class);
-Route::resource('/prodi', ProdiController::class);
-Route::resource('/mahasiswa', MahasiswaController::class);
+require __DIR__ . '/auth.php';
